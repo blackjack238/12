612275cc6d14456be579be825d3633ade4bc19b1
blackjack238 <118196030+blackjack238@users.noreply.github.com>
2023-04-27 12:44:54 +0300

    2

---
+++ src/main/java/org/example/Name.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Name {
+    private String NAME;
+    private String LASTNAME;
+
+    public Name(String NAME, String LASTNAME) {
+        this.NAME = NAME;
+        this.LASTNAME = LASTNAME;
+    }
+}
